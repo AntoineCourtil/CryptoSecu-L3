78671591cde19b239d383b19da396fa9566fc72a
AntoineCourtil <courtil.antoine@hotmail.fr>
2017-03-29 15:20:15 +0200

    bug fix && fsm OK

--- lfsr.c
+++ lfsr.c
@@ -3,12 +3,12 @@
 void init_lfsr_state_test(E0 *state) {
   
   state->lfsr_1_4 = (uint64_t)0x04B583D;
-  state->lfsr_1_4 += ((uint64_t) 0x0F7A2A42BB << 25);
   state->lfsr_2_3 =  (uint64_t)0x208E1EC1;
   state->lfsr_2_3 +=  ((uint64_t) 0x063C142F0 << 31);
+  state->lfsr_1_4 += ((uint64_t) 0x0F7A2A42BB << 25);
 
-  //state->fsm_memory = 3; //  C[t] <= 11
-  //state->fsm_memory += (1 << 2); // C[t-1] <= 01
+  state->fsm = 3;
+  state->fsm += (1 << 2);
 }
 
 void print_lfsr1(E0 *state) {
@@ -31,7 +31,6 @@ void print_lfsr1(E0 *state) {
         printf(" ");
     }
     if(i == 24 || i == 19 || i == 11 || i == 7){
-
       printf("x ");
     }
     else{
@@ -63,7 +62,6 @@ void print_lfsr2(E0 *state) {
         printf(" ");
     }
     if(i == 30 || i == 23 || i == 15 || i == 11){
-
       printf("x ");
     }
     else{
@@ -95,7 +93,6 @@ void print_lfsr3(E0 *state) {
         printf(" ");
     }
     if(i == 32 || i == 27 || i == 23 || i == 11){
-
       printf("x ");
     }
     else{
@@ -130,7 +127,6 @@ void print_lfsr4(E0 *state) {
         printf(" ");
     }
     if(i == 38 || i == 35 || i == 27 || i == 3){
-
       printf("x ");
     }
     else{
@@ -142,23 +138,40 @@ void print_lfsr4(E0 *state) {
   
 }
 
-void cycle_lfsr(E0 *state) {
-  uint8_t xor, buffer;
+void print_E0(E0 *state){
+  /*print_lfsr1(state);
+  print_lfsr2(state);
+  print_lfsr3(state);
+  print_lfsr4(state);*/
 
-  buffer = 0;
+  printf("    X1 = %lu\n", get_bit(state->lfsr_1_4, 23));
+  printf("    X2 = %lu\n", get_bit(state->lfsr_2_3, 23));
+  printf("    X3 = %lu\n", get_bit(state->lfsr_2_3, 31+31));
+  printf("    X4 = %lu\n", get_bit(state->lfsr_1_4, 36+25));
+
+  printf("Yt 0x%x\n" , (state->fsm & 0x70) >> 4);
+  printf("Ct 0x%x\n" , state->fsm & 0x03);
+  printf("Ct-1 0x%x\n" , (state->fsm & 0x0c) >> 2);
+}
+
+void cycle_lfsr(E0 *state) {
+
+  printf("\n-----------------\n");
+
+  uint8_t xor;
 
   //LFSR1
 
   xor = get_xor_lfsr_1((uint64_t)state->lfsr_1_4);
 
-  printf("LFSR1 : %lu %lu %lu %lu\n", 
+  /*printf("LFSR1 : %lu %lu %lu %lu\n", 
     get_bit(state->lfsr_1_4, 24),
     get_bit(state->lfsr_1_4, 19),
     get_bit(state->lfsr_1_4, 11),
     get_bit(state->lfsr_1_4, 7)
   );
 
-  printf("XOR LFSR 1 : %d\n", xor);
+  printf("XOR LFSR 1 : %d\n\n", xor);*/
   set_bit(state->lfsr_1_4, 0, xor);
 
 
@@ -167,7 +180,7 @@ void cycle_lfsr(E0 *state) {
 
   xor = get_xor_lfsr_2((uint64_t)state->lfsr_2_3);
 
-  printf("LFSR2 : %lu %lu %lu %lu\n", 
+  /*printf("LFSR2 : %lu %lu %lu %lu\n", 
     get_bit(state->lfsr_2_3, 30),
     get_bit(state->lfsr_2_3, 23),
     get_bit(state->lfsr_2_3, 15),
@@ -175,7 +188,7 @@ void cycle_lfsr(E0 *state) {
   );
 
   
-  printf("XOR LFSR 2 : %d\n", xor);
+  printf("XOR LFSR 2 : %d\n\n", xor);*/
   set_bit(state->lfsr_2_3, 0, xor);
 
 
@@ -184,15 +197,15 @@ void cycle_lfsr(E0 *state) {
 
   xor = get_xor_lfsr_3((uint64_t)state->lfsr_2_3);
 
-  printf("LFSR3 : %lu %lu %lu %lu\n", 
+  /*printf("LFSR3 : %lu %lu %lu %lu\n", 
     get_bit(state->lfsr_2_3, 31+32),
     get_bit(state->lfsr_2_3, 31+27),
     get_bit(state->lfsr_2_3, 31+23),
     get_bit(state->lfsr_2_3, 31+11)
   );
 
-  printf("XOR LFSR 3 : %d\n", xor);
-  set_bit(state->lfsr_2_3, 31, xor);
+  printf("XOR LFSR 3 : %d\n\n", xor);*/
+  set_bit(state->lfsr_2_3, 30, xor);
 
 
 
@@ -200,7 +213,7 @@ void cycle_lfsr(E0 *state) {
 
   xor = get_xor_lfsr_4((uint64_t)state->lfsr_1_4);
 
-  printf("LFSR2 : %lu %lu %lu %lu\n", 
+  /*printf("LFSR4 : %lu %lu %lu %lu\n", 
     get_bit(state->lfsr_1_4, 25+38),
     get_bit(state->lfsr_1_4, 25+35),
     get_bit(state->lfsr_1_4, 25+27),
@@ -209,49 +222,99 @@ void cycle_lfsr(E0 *state) {
 
   
   
-  printf("XOR LFSR 4 : %d\n", xor);
+  printf("XOR LFSR 4 : %d\n", xor);*/
   set_bit(state->lfsr_1_4, 25, xor);
+
+  left_shift(state->lfsr_1_4);
+  left_shift(state->lfsr_2_3);
+
+
+  printf("-----------------\n\n");
 }
+
+
+void calcul_fsm(E0 *state){
+  uint8_t st_1, ct_1, yt;
+
+  st_1 = 0;
+  ct_1 = 0;
+
+  yt = get_bit(state->lfsr_1_4, 23) + get_bit(state->lfsr_2_3, 23) + get_bit(state->lfsr_2_3, 31+31) + get_bit(state->lfsr_1_4, 25+37) ;
+
+  printf("yt = %d\n", yt);
+
+  state->out = get_bit(yt, 0);
+
+  st_1 = 2 * get_bit(state->fsm, 6) + get_bit(state->fsm, 5);
+
+  printf("st+1 = %d\n", st_1);
+
+  set_bit(ct_1, 1,
+    get_bit(st_1, 1) ^
+    get_bit(state->fsm, 1) ^
+    get_bit(state->fsm, 2)
+  );
+
+  set_bit(ct_1, 0, get_bit(st_1, 0) ^ 
+    get_bit(state->fsm, 0) ^
+    get_bit(state->fsm, 3) ^
+    get_bit(state->fsm, 2)
+  );
+
+
+
+  // Ct-1 = Ct
+  state->fsm <<= 2;
+
+  // Yt = 0 et Ct = 0
+  state->fsm &= 0x0C;
+
+  // Ct = Ct+1
+  state->fsm += ct_1;
+
+  // maj Yt
+  state->fsm += (yt << 4);
+}
+
+uint8_t next_bit(E0 *state){
+  calcul_fsm(state);
+  cycle_lfsr(state);
+
+  return state->out;
+}
+
 
 int test() {
 
   int i ;
   E0 state;
-  uint8_t res = 0;
   uint8_t bit;
 
   init_lfsr_state_test(&state);
 
-  print_lfsr1(&state);
-  print_lfsr2(&state);
-  print_lfsr3(&state);
-  print_lfsr4(&state);
+  printf("\n\n========== TOUR 1 =============\n\n");
+  print_E0(&state);
 
-  /*for (i = 0; i < 4; i++) {
-    bit = nextBit(&state);
-    printf("%d ", bit);
-    res += (bit << i);
-  }*/
+  for (i = 2; i < 15; i++) {
+  printf("\n\n========== TOUR %d =============\n\n", i);
+    bit = next_bit(&state);
+    print_E0(&state);
+  }
+
+  /*cycle_lfsr(&state);
+  print_E0(&state);*/
 
   printf("\n");
 
-  cycle_lfsr(&state);
 
-
-  
-
-  print_lfsr1(&state);
-  print_lfsr2(&state);
-  print_lfsr3(&state);
-  print_lfsr4(&state);
-
-  return res == 2;
+  return 0;
 }
 
 
 
-void main()
-{
+
+int main() {
+
    printf("\n[+] Test LFSR : \n\n");
 
   test();
--- lfsr.h
+++ lfsr.h
@@ -16,32 +16,46 @@ struct E0_state
 	uint64_t lfsr_2_3;
 
 	// memoire fsm
-	uint8_t memoire_fsm;
+	uint8_t fsm;
+
+	// sortie fsm
+	uint8_t out;
 };
 
 typedef struct E0_state E0;
 
-#define get_bit(reg, no) ( ( (uint64_t)reg & (1 << ((uint64_t)no)) ) >> ((uint64_t)no) )
-#define get_xor_lfsr_1(reg) ( get_bit((uint64_t)reg, 24) ^ get_bit((uint64_t)reg, 19) ^ get_bit((uint64_t)reg, 11) ^ get_bit((uint64_t)reg, 7) )
-#define get_xor_lfsr_2(reg) ( get_bit((uint64_t)reg, 30) ^ get_bit((uint64_t)reg, 23) ^ get_bit((uint64_t)reg, 15) ^ get_bit((uint64_t)reg, 11) )
-#define get_xor_lfsr_3(reg) ( get_bit((uint64_t)reg, 32+31) ^ get_bit((uint64_t)reg, 27+31) ^ get_bit((uint64_t)reg, 23+31) ^ get_bit((uint64_t)reg, 11+31) )
-#define get_xor_lfsr_4(reg) ( get_bit((uint64_t)reg, 38+25) ^ get_bit((uint64_t)reg, 35+25) ^ get_bit((uint64_t)reg, 27+25) ^ get_bit((uint64_t)reg, 3+25) )
+#define get_bit(reg, no) ( ( reg & ((uint64_t)1 << ((uint64_t)no)) ) >> ((uint64_t)no) )
+#define get_xor_lfsr_1(reg) ( get_bit(reg, (uint64_t)24) ^ get_bit((uint64_t)reg, (uint64_t)19) ^ get_bit((uint64_t)reg, (uint64_t)11) ^ get_bit((uint64_t)reg, (uint64_t)7) )
+#define get_xor_lfsr_2(reg) ( get_bit(reg, (uint64_t)30) ^ get_bit((uint64_t)reg, (uint64_t)23) ^ get_bit((uint64_t)reg, (uint64_t)15) ^ get_bit((uint64_t)reg, (uint64_t)11) )
+#define get_xor_lfsr_3(reg) ( get_bit(reg, (uint64_t)32+(uint64_t)31) ^ get_bit((uint64_t)reg, (uint64_t)27+(uint64_t)31) ^ get_bit((uint64_t)reg, (uint64_t)23+(uint64_t)31) ^ get_bit((uint64_t)reg, (uint64_t)11+(uint64_t)31) )
+#define get_xor_lfsr_4(reg) ( get_bit(reg, (uint64_t)38+(uint64_t)25) ^ get_bit((uint64_t)reg, (uint64_t)35+(uint64_t)25) ^ get_bit((uint64_t)reg, (uint64_t)27+(uint64_t)25) ^ get_bit((uint64_t)reg, (uint64_t)3+(uint64_t)25) )
 
 
-#define set_bit(r, no, val) ( r = (r &  ~(1 << (uint64_t)no)) | ((uint64_t)val << (uint64_t)no))
-#define set_0_lfsr_1(reg) ( set_bit((uint64_t)reg, 0, 0) )
-#define set_0_lfsr_2(reg) ( set_bit((uint64_t)reg, 0, 0) )
-#define set_0_lfsr_3(reg) ( set_bit((uint64_t)reg, 31, 0) )
-#define set_0_lfsr_4(reg) ( set_bit((uint64_t)reg, 25, 0) )
+#define set_bit(reg, no, val) ( reg = (reg &  ~((uint64_t)1 << (uint64_t)no)) | ((uint64_t)val << (uint64_t)no))
+#define set_0_lfsr_1(reg) ( set_bit(reg, 0, 0) )
+#define set_0_lfsr_2(reg) ( set_bit(reg, 0, 0) )
+#define set_0_lfsr_3(reg) ( set_bit(reg, 31, 0) )
+#define set_0_lfsr_4(reg) ( set_bit(reg, 25, 0) )
+
+#define left_shift(r) (r = r << 1)
 
 
 void init_lfsr_state_test(E0 *state);
+
 void print_lsfr1(E0 *state);
 void print_lsfr2(E0 *state);
 void print_lsfr3(E0 *state);
 void print_lsfr4(E0 *state);
+void print_E0(E0 *state);
+
 int test();
 void lfsr();
+
+void cycle_lfsr(E0 *state);
+void calcul_fsm(E0 *state);
+
+uint8_t next_bit(E0 *state);
+
 
 #endif
 
